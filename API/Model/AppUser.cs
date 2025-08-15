2ae4aa0fb02a19787abc7d8554aa7d842a3b3098
Hossein Ghelichkhani <60262273+hosseinghDev@users.noreply.github.com>
2025-08-15 13:32:15 +0330

    Add project files.

---
+++ API/Model/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+    }
+}
